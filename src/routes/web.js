b64ab0332deba7c84105f5e257a5c697b018209d
Tuananh224z <siuobito@gmail.com>
2026-07-12 16:12:50 +0700

    video 3

--- src/routes/web.js
+++ src/routes/web.js
@@ -1,11 +1,11 @@
 import express from 'express';
-
+import homeController from '../controllers/homeController';
 const router = express.Router();
 
+
 const initWebRoutes = (app) => {
-  router.get('/', (req, res) => {
-    return res.send('Hello World! ')
-  })
+  router.get('/',homeController.handleHelloWorld);
+  router.get('/user',homeController.handleUserpage);
   return app.use('/', router);
 };
 
